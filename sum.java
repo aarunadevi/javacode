3f42be953cd5dc45ecb31aa12ef9de540df16b93
aarunadevi <aarunadevi07@gmail.com>
2017-06-21 09:35:55 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class sum {
+	public static void main(String[] args) 
+	    {
+		System.out.println("Enter the value of N");
+		int n,i,sum=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(i=0;i<=n;i++){
+			sum=sum+i;
+		}
+		System.out.println("Sum of N numbers is:"+sum);
+	     }
+	}
