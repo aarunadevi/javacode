16372973ec270a4cdba5b407a4fc414c0fcc9456
aarunadevi <aarunadevi07@gmail.com>
2017-06-22 11:19:08 +0530

    Create strgtoint.java

---
+++ strgtoint.java
@@ -0,0 +1,11 @@
+package positive;
+import java.util.*;
+public class strgtoint {
+	public static void main(String[] arg)
+	{
+		String s="200";
+		int i=Integer.parseInt(s);
+		System.out.println(s+100);
+		System.out.println(i+200);
+	}
+}
